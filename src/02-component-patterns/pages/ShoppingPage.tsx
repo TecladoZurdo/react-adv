aaedf6ce2a35206742fa1cdf3d2c6ddfefe9bef2
ehernandez <ehernandez@metropolitan-touring.com>
2021-12-08 18:56:38 -0500

    feat: inicio

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,4 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
-import '../styles/custom-styles.css';
 import { products } from '../data/products';
 
 const product = products[0];
@@ -10,23 +9,17 @@ export const ShoppingPage = () => {
             <hr />
             <ProductCard key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
-                    count: 4,
-                    maxCount: 10,
+                    count: 2,
+                    // maxCount: 10,
                 }}
             >
                 {
                     ({reset,count,isMaxCountReached,increaseBy}) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-                            <button onClick={reset}>Reset</button>
-                            <button onClick={()=>{increaseBy(-2)}}>-2</button>
-                            {!isMaxCountReached &&<button onClick={()=>increaseBy(2)}>+2</button>}
-                            <span>{count}</span>
-                            
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
